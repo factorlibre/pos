f1509fddb8e8fa7b954681ed36cea979ac2c430d
sushan <sushan.voong@factorlibre.com>
2025-10-23 16:47:56 +0200

    [16.0][IMP] pos_picking_delayed: The setting 'Picking Creation Delayed' is only compatible with 'Inventory Management' in real time.

--- pos_picking_delayed/models/res_config_settings.py
+++ pos_picking_delayed/models/res_config_settings.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
@@ -11,3 +11,17 @@ class ResConfigSettings(models.TransientModel):
     picking_creation_delayed = fields.Boolean(
         related="pos_config_id.picking_creation_delayed", readonly=False
     )
+
+    @api.onchange("picking_creation_delayed", "update_stock_quantities")
+    def _onchange_picking_creation_delayed(self):
+        if self.picking_creation_delayed and self.update_stock_quantities == "closing":
+            self.picking_creation_delayed = False
+            return {
+                "warning": {
+                    "title": _("Picking Creation Delayed Warning"),
+                    "message": _(
+                        "The setting 'Picking Creation Delayed' is only compatible "
+                        "with 'Inventory Management' in real time."
+                    ),
+                }
+            }
